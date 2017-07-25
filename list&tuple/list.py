77abc1342b910dfe8f20d3555ec73e4ce0b6f852
cxh-er <1844608972@qq.com>
2017-07-25 22:32:20 +0800

    list&tuple

---
+++ list&tuple/list.py
@@ -0,0 +1,16 @@
+
+a=[1,2,3]
+print(a[1])
+print(a[-1])
+a.append(4)
+print(a[-1])
+a.pop()
+print(a[-1])
+print(a[1])
+a.pop(1)
+print(a[1])
+a[1]=45
+print(a[1])
+a=[1,[2,3],4]
+print(a)
+print(a[1][0])
