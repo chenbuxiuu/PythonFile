32c3c3dfd1e49af2307c3fd20c0415b804c628d3
cxh-er <1844608972@qq.com>
2017-07-31 23:08:26 +0800

    High-oder function

--- fun/fun.py
+++ fun/fun.py
@@ -1,4 +1,5 @@
 from quadratic import quadratic
+from HigherOrderFunction import PositiveIntegerAdd
 a=input("a=")
 a=float(a)
 b=input("b=")
@@ -7,3 +8,7 @@ c=input("c=")
 c=float(c)
 
 quadratic(a,b,c)
+
+x=-1
+y=2
+print("abs(%d)+abs(%d)="%(x,y),PositiveIntegerAdd(x,y,abs))
