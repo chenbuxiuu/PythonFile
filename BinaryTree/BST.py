a3640c2c7a72820eb465a8630dfe963fddd0a6ba
cxh-er <1844608972@qq.com>
2018-01-30 09:50:12 +0800

    BST

--- BinaryTree/BST.py
+++ BinaryTree/BST.py
@@ -1,4 +1,5 @@
 import copy
+import random
 class Node:
     def __init__(self,value):
         self.value = value
@@ -18,16 +19,6 @@ class Tree:
     def __init__(self):
         self.root=None
 
-    # # check para
-    # def checkIsNode(self,node):
-    #     if not isinstance(node, Node):
-    #         n = Node(node)
-    #         # print('make %d to be class Node'%node)
-    #         return n
-    #     else:
-    #         # print('%d is a value of a class Node'%(node.value))
-    #         return node
-
     # search  specified key from tree
     def  search(self,parent,k):
         if parent==None:
@@ -39,64 +30,77 @@ class Tree:
         elif parent.value==k:
             return True,parent
 
+    # search parent node
+    def search_parent(self,parent,k):
+        if parent==None:
+            return False,None
+        if (parent.leftChild!=None and parent.leftChild.value==k) or (parent.rightChild!=None and parent.rightChild.value==k):
+            return True,parent
+        elif parent.value<k:
+            return self.search_parent(parent.rightChild,k)
+        elif parent.value>k:
+            return self.search_parent(parent.leftChild,k)
+
 
 
 
     # insert node to tree
-    def insert(self,value):
+    def insert(self,value,root):
         if not isinstance(value, Node):
             node= Node(value)
-
         if self.root==None:
             self.root=node
+            root=self.root
+            return root
+        if root==None:
+            root=node
         else :
-            parent=self.root
-            child=None
-            # insert right subtree
-            if parent.value<node.value:
-                while 1:
-                    current=parent.rightChild
-                    if current==None :  
-                        parent.rightChild=node
-                        break
-                    elif current.value>node.value:
-                        node.rightChild=parent.rightChild
-                        parent.rightChild=node
-                        break
-                    else:
-                        parent=parent.rightChild
-            # insert left subtree
-            if parent.value>node.value:
-                while 1:
-                    current=parent.leftChild
-                    if current==None:
-                        parent.leftChild=node
-                        break
-                    elif current.value<node.value:  
-                        node.leftChild=parent.leftChild
-                        parent.leftChild=node
-                        break                  
-                    else:
-                        parent=parent.leftChild
+            if value<root.value:
+                root.leftChild=self.insert(value,root.leftChild)
+            else:
+                root.rightChild=self.insert(value,root.rightChild)
+        return root
+
+    # find min
+    def findmin(self,node):  
+        if node.leftChild!=None:  
+            return self.findmin(node.leftChild);  
+        else:  
+            return node; 
 
 
 
     # delete node
-    def delete(self,parent,value):
-        if self.root is None:  
-            print('Tree is null')
-        if parent.leftChild is None:
-            print('111') 
-            parent=parent.rightChild
-        elif parent.rightChild is None:
-            print('222') 
-            parent=parent.leftChild
+    def delete(self,k):
+        # delete ndoe is root
+        if k==self.root.value:
+            if self.root.rightChild!=None:
+                tmp=self.findmin(self.root.rightChild)
+            self.delete(tmp.value)
+            self.root.value=tmp.value      
         else:
-            print('333')                                             # 删除要区分左右孩子是否为空的情况    
-            tmp=parent.rightChild;             #找到后继结点  
-            parent.value=tmp.value;  
-            parent.rightChild=self.delete(parent.rightChild,value)    #实际删除的是这个后继结点  
-
+            # find the target node
+            f,node=self.search(self.root,k)
+            # find the target node's parent
+            pf,pnode=self.search_parent(self.root,k)
+            if f and pf:
+                # 删除node没有左子树
+                if node.leftChild is None:
+                    if pnode.value<node.value:
+                        pnode.rightChild=node.rightChild
+                    else:
+                        pnode.leftChild=node.rightChild
+                # 删除node没有右子树
+                elif node.rightChild is None:
+                    if pnode.value<node.value:
+                        pnode.rightChild=node.leftChild
+                    else:
+                        pnode.leftChild=node.leftChild
+                # 删除node有左右子树
+                else:
+                    tmp=self.findmin(node.rightChild)
+                    self.delete(tmp.value)
+                    node.value=tmp.value
 
 
     # traverse
@@ -113,40 +117,68 @@ class Tree:
         if self.root is None:
             return []
         q = [self.root]
-        res = [self.root.value]
+        
+        level=[self.root.value]
         while q != []:
-            pop_node = q.pop(0)
-            if pop_node.leftChild is not None:
-                q.append(pop_node.leftChild)
-                res.append(pop_node.leftChild.value)
+            for ii in range(len(q)):
+                pop_node = q.pop(0)
+                res = []
+                if pop_node.leftChild is not None:
+                    q.append(pop_node.leftChild)
+                    res.append(pop_node.leftChild.value)
 
-            if pop_node.rightChild is not None:
-                q.append(pop_node.rightChild)
-                res.append(pop_node.rightChild.value)
-        return res
+                if pop_node.rightChild is not None:
+                    q.append(pop_node.rightChild)
+                    res.append(pop_node.rightChild.value)
+                level.append(res)
+        print('print Tree by level+++++++++++++++++++++++++')
+        for tlevel in level:
+            print(tlevel)
+        print('end Tree------------------------------------')
+        return level
+
 
         
 
+if __name__ == '__main__':
+
+    t=Tree()
+
+    array=[]
+    for ii in range(100):
+        array.append(ii)
+    # print(array)
 
 
-t=Tree()
-t.insert(2)
-t.insert(1)
-# t.insert(5)
-t.insert(6)
-t.insert(5)
-t.insert(7)
-t.insert(4)
-
-# r=t.preorder(t.root)
-# for item in r:
-#     item.showNodeInfo()
+    # tree_array=random.sample(array,10)
+    tree_array=[75,97,26,18,66,88,5,55,72,32]
+    print(tree_array)
 
 
-print(t.traverse())
-t.delete(t.root,1)
+    # t.root=Node(12)
+    for ii in tree_array:
+        t.insert(ii,t.root)
 
-print(t.traverse())
+    r=t.preorder(t.root)
+    for item in r:
+        item.showNodeInfo()
+
+    t.traverse()
+    delt=tree_array[0]
+    print(delt)
+    t.delete(delt)
+    t.traverse()
+# level=t.traverse()
+
+    # for node in tlevel:
+    #     print(ndoe)
+# print(level)
+# t.delete(2)
+
+# t.traverse()
+
+
+
 
 
 
